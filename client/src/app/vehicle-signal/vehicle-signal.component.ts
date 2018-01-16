86764112ccf12d64eee1ed36db1ade36bb7958df
dipeshnainani <dipeshnainani@gmail.com>
2018-01-16 01:25:46 -0500

    feat(module-client): add feature for specific signal

---
+++ client/src/app/vehicle-signal/vehicle-signal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-signal',
+  templateUrl: './vehicle-signal.component.html',
+  styleUrls: ['./vehicle-signal.component.css']
+})
+export class VehicleSignalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
