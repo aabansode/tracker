5f3b823bb3fd68ee571a16a3e66bb76c069c113e
dipeshnainani <dipeshnainani@gmail.com>
2018-01-16 01:28:46 -0500

    feat(module-client): add feature to get all high alerts

---
+++ client/src/app/alerts-allvehicles/alerts-allvehicles.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alerts-allvehicles',
+  templateUrl: './alerts-allvehicles.component.html',
+  styleUrls: ['./alerts-allvehicles.component.css']
+})
+export class AlertsAllvehiclesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
