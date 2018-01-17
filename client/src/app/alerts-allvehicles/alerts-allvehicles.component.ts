b33a2080e8f0c7b079420737686f89e457de6b5f
dipeshnainani <dipeshnainani@gmail.com>
2018-01-16 21:23:31 -0500

    refractor(module-client): modified all alerts component to get high alerts

--- client/src/app/alerts-allvehicles/alerts-allvehicles.component.ts
+++ client/src/app/alerts-allvehicles/alerts-allvehicles.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CarService} from '../car-service/car.service.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-alerts-allvehicles',
@@ -7,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlertsAllvehiclesComponent implements OnInit {
 
-  constructor() { }
-
+  cars;
+  constructor(private carService: CarService, private route: ActivatedRoute) {
+    this.route.params.subscribe(params => {
+      this.carService.getVehicleWithType('HIGH')
+        .subscribe(cars => this.cars = cars);
+    });
+  }
   ngOnInit() {
   }
 
